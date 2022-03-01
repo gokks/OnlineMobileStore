ef7580abb9aa5674d8ffd4fe52d4b4f910b0c94b
gokks <gokulrajpdpm@gmail.com>
2022-03-01 22:36:38 +0530

    initial commit

---
+++ MobileStore/Controllers/CustomerController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MobileStore.Models;
+
+namespace MobileStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Mobile_StoreEntities db = new Mobile_StoreEntities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Customer_Id,Name,Mail_Id,Phone_Number,Address,Username,Password")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Customer_Id,Name,Mail_Id,Phone_Number,Address,Username,Password")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult Login(Customer customer)
+        {
+            Mobile_StoreEntities mobile_StoreEntities = new Mobile_StoreEntities();
+            for(int i=0;i<mobile_StoreEntities.Customers.Count();i++)
+            {
+                if(customer.Username==mobile_StoreEntities.Customers.ToList()[i].Username)
+                {
+                    if(customer.Password == mobile_StoreEntities.Customers.ToList()[i].Password)
+                    {
+                        //redirect to action
+                        return Content("Login Success");
+                    }
+                    else
+                    {
+                        //return view()
+                        return Content("Password Wrong");
+                    }
+                }
+            }
+            //return view()
+            return Content("User Id not found");
+        }
+    }
+}
---
+++ MobileStore/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using MobileStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileStore.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult Login(Customer customer)
+        {
+            int customerid = 0;
+            string password = "";
+            Mobile_StoreEntities db = new Mobile_StoreEntities();
+            foreach(Customer customer1 in db.Customers)
+            {
+                if(customer1.Username == customer.Username)
+                {
+                    customerid=customer1.Customer_Id;
+                    password = customer1.Password;
+                    break;
+                }
+            }
+            if(customerid == 0)
+            {
+                ViewData["NoUser"] = "User Not Found";
+                return View(customer);
+            }
+            else
+            {
+                if (password == customer.Password)
+                {
+                    return Content("Login Success");
+                }
+                else
+                {
+                    ViewData["WrongPass"] = "Wrong Password";
+                    return View(customer);
+                }
+            }
+        }
+    }
+}
